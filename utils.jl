4210cdfcfbec0f26c7aa6f193988937681090774
shashi <gowda@mit.edu>
2020-09-02 12:03:16 +0000

    Deploying to gh-pages from  @ fe6e28e2209619427b7d1f39a5d9eec1ff5947f2 🚀

--- utils.jl
+++ utils.jl
@@ -1,8 +1,8 @@
 # Thanks @tlienart
 
-using Franklin, PkgPage
+using Franklin, PkgPage, JSON
 
-using Markdown
+using Markdown, Dates
 
 include("youtube_videos.jl")
 
@@ -27,10 +27,85 @@ end
 function hfun_youtube(params)
     id = params[1]
     return """
-    <iframe width="800" height="474"
+    <iframe id="$id" width="800" height="474"
     src="https://www.youtube.com/embed/$(get(videos, id, id))"
     frameborder="0"
     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen></iframe>
+    """
+end
+
+function hfun_showtime(params)
+    id = params[1]
+    str = locvar(id)
+    if isnothing(str)
+        @warn "Unknown datetime variable $str"
+        return ""
+    end
+    try
+        DateTime(str, dateformat"yyyy-mm-dd HH:MMp")
+    catch err
+        @warn "There was an error parsing date $str, the format is yyyy-mm-dd HH:MMp (see ?DateFormat)"
+        rethrow(err)
+    end
+end
+
+
+function parse_duration(str)
+    str = replace(str, r"^PT"=>"")
+    hrex, mrex, srex = Regex.(string.("^([0-9]+)", ["H","M","S"]))
+
+    t = 0
+    hmatch = match(hrex, str)
+    if !isnothing(hmatch)
+        h = parse(Int, hmatch[1])
+        t += 60*60*h
+        str = replace(str, hrex=>"")
+    end
+
+    mmatch = match(mrex, str)
+    if !isnothing(mmatch)
+        m = parse(Int, mmatch[1])
+        t += 60*m
+        str = replace(str, mrex=>"")
+    end
+
+    smatch = match(srex, str)
+    if !isnothing(smatch)
+        s = parse(Int, smatch[1])
+        t += s
+        str = replace(str, srex=>"")
+    end
+
+    t
+end
+
+function hfun_go_live()
+    seq = locvar("sequence")
+    airtime = locvar("airtime")
+
+    if isnothing(seq)
+        @warn "airtime set, but no `sequence` variable not defined." *
+        "sequence is an array of video IDs to play in order on this page"
+    end
+
+    vid_ids = [get(videos, s, s) for s in seq]
+
+    f = tempname()
+    # Get the duration of each video
+    download("https://www.googleapis.com/youtube/v3/videos?id=$(join(vid_ids, ","))&part=contentDetails&key=AIzaSyDZhbWHc2PTEFTx173MaTgddnWCGPqdbB8", f)
+    dict = JSON.parse(String(read(f)))
+
+    durations = [parse_duration(video["contentDetails"]["duration"])
+                 for video in dict["items"]]
+
+    csum = cumsum(durations)
+
+    jrepr(x) = sprint(io->JSON.print(io, x))
+    """
+    <script src="/assets/live-player.js"></script>
+    <script>
+    play_live($(jrepr(airtime)), $(jrepr(seq)), $(jrepr(csum)))
+    </script>
     """
 end
