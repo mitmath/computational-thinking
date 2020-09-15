a347226afb69a2e9b24747546dc925f8a211febe
Shashi Gowda <gowda@mit.edu>
2020-09-15 14:09:15 -0400

    structure

--- website/youtube_videos.jl
+++ website/youtube_videos.jl
@@ -15,6 +15,7 @@ videos = Dict(
               "james-image-seam" => "ALcohd1q3dk",
               "conv-gpus"    => "aa3JkX_cj_I",
               "conv-real-world" => "e6WnzfvnFlY",
+              "structure"    => "yU8P_cHPn8Q",
               "types"        => "yU8P_cHPn8Q",
               "dispatch"     => "DXtbaSP_LFI",
               "ides"         => "PgeLMzw_-Ww",
