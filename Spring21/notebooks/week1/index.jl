e62d9e51ec467af6fe6f705d5ea1e66558566bf4
fonsp <fonsvdplas@gmail.com>
2021-02-08 14:15:33 +0000

    Deploying to Spring21-franklin-output from  @ 91c983c2b1b0a081f33b15b6fb71ebca502bf966 🚀

--- Spring21/notebooks/week1/index.jl
+++ Spring21/notebooks/week1/index.jl
@@ -17,7 +17,7 @@ end
 begin
 	import Pkg
 	Pkg.activate(mktempdir())
-	Pkg.add(["Images", "ImageIO", "Colors", "PlutoUI"])
+	Pkg.add(["Images", "ImageMagick", "Colors", "PlutoUI"])
 
 	using Images
 	using Colors
