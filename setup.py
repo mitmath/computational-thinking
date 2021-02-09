e2dec90d56e0968e4ba307fe8aad56c2c8c5dcb6
Fons van der Plas <fonsvdplas@gmail.com>
2021-02-09 23:03:43 +0100

    install Pluto from the python script UGH

--- setup.py
+++ setup.py
@@ -14,6 +14,10 @@ setuptools.setup(
   dependency_links=['http://github.com/fonsp/jupyter-server-proxy/tarball/3a58aa5005f942d0c208eab9a480f6ab171142ef#egg=jupyter-server-proxy-1.5.1002']
 )
 
-# we add some common packages to trigger download and precompile
 import os
-os.system('julia -e "import Pkg; Pkg.activate(mktempdir()); Pkg.add([\\"Images\\", \\"ImageMagick\\", \\"Plots\\"]); Pkg.precompile()"')
+
+# add Pluto. We don't use Manifest.toml because of some package server issues
+os.system('julia -e "import Pkg; Pkg.Registry.update(); Pkg.activate(); Pkg.add(Pkg.PackageSpec(name=\\"Pluto\\", rev=\\"82f23e9\\"));"')
+
+# we add some common packages to trigger download and precompile
+os.system('julia -e "import Pkg; Pkg.activate(); Pkg.add(Pkg.PackageSpec(name=\\"Pluto\\", rev=\\"82f23e9\\")); Pkg.activate(mktempdir()); Pkg.add([\\"Images\\", \\"ImageMagick\\", \\"Plots\\"]); Pkg.precompile()"')
