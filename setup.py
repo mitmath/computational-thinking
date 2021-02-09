d11dfc8d67be576d0514fbbcda8070f730fab237
Fons van der Plas <fonsvdplas@gmail.com>
2021-02-09 22:44:25 +0100

    do precompilation differently

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setuptools.setup(
   dependency_links=['http://github.com/fonsp/jupyter-server-proxy/tarball/3a58aa5005f942d0c208eab9a480f6ab171142ef#egg=jupyter-server-proxy-1.5.1002']
 )
 
-# because this is a demo of Pluto, we add some popular packages to the global package env and precompile
+# we add some common packages to trigger download and precompile
 import os
-os.system('julia -e "import Pkg; Pkg.add([\\"DataFrames\\", \\"CSV\\", \\"Plots\\"]); Pkg.precompile()"')
+os.system('julia -e "import Pkg; Pkg.activate(mktempdir()); Pkg.add([\\"Images\\", \\"ImageMagick\\", \\"Plots\\"]); Pkg.precompile()"')
