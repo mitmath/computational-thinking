3f3f4480ab4e8c52af861cf1c7d062fb8d1a7882
Alan Edelman <edelman.alan@gmail.com>
2021-02-23 15:22:29 -0500

    Update linear_transformation.jl
    add scrubbable

--- notebooks/week2/linear_transformation.jl
+++ notebooks/week2/linear_transformation.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.12.20
+# v0.12.21
 
 using Markdown
 using InteractiveUtils
@@ -12,6 +12,11 @@ macro bind(def, element)
         el
     end
 end
+
+# ╔═╡ 2e8c4a48-d535-44ac-a1f1-4cb26c4aece6
+filter!(LOAD_PATH) do path
+	path != "@v#.#"
+end;
 
 # ╔═╡ 6b473b2d-4326-46b4-af38-07b61de287fc
 begin
@@ -30,30 +35,41 @@ begin
 	using LinearAlgebra
 end
 
-# ╔═╡ 2e8c4a48-d535-44ac-a1f1-4cb26c4aece6
-filter!(LOAD_PATH) do path
-	path != "@v#.#"
-end;
-
 # ╔═╡ 60532aa0-740c-11eb-0402-af8ff117f042
 md"Show grid lines $(@bind show_grid CheckBox(default=true))"
 
 # ╔═╡ 35904b8e-7a28-4dbc-bbf9-b45da448452c
+# let
+
+# 	range = -1.5:.1:1.5
+# 	md"""
+	
+# 	a $(@bind a Slider(range; default=1.0, show_value=true))
+	
+# 	b $(@bind b Slider(range; default=0.0, show_value=true))
+	
+# 	c $(@bind c Slider(range; default=0.0, show_value=true))
+	
+# 	d $(@bind d Slider(range; default=1.0, show_value=true))
+	
+# 	**Re-run this cell to reset to identity transformation**
+# 	"""
+# end
+
+# ╔═╡ e2c9600e-7613-11eb-0c5b-492127ac0817
 let
 
-	range = -1.5:.1:1.5
-	md"""
+range = -1.5:.1:1.5
+md"""
 	
-	a $(@bind a Slider(range; default=1.0, show_value=true))
-	
-	b $(@bind b Slider(range; default=0.0, show_value=true))
-	
-	c $(@bind c Slider(range; default=0.0, show_value=true))
-	
-	d $(@bind d Slider(range; default=1.0, show_value=true))
+a $(@bind a Scrubbable( range; default=1.0))
+b $(@bind b Scrubbable( range; default=0.0))
+
+c $(@bind c Scrubbable(range; default=0.0 ))
+d $(@bind d Scrubbable(range; default=1.0))
 	
 	**Re-run this cell to reset to identity transformation**
-	"""
+"""
 end
 
 # ╔═╡ f085296d-48b1-4db6-bb87-db863bb54049
@@ -130,10 +146,13 @@ end;
 
 # ╔═╡ 8e0505be-359b-4459-9de3-f87ec7b60c23
 [
-	if det_A == 0
+	if abs(det_A) < 1e-4
 		RGB(1.0,1.0,1.0)
 	else
+
+		
 		in_x, in_y = A \ [out_x, out_y]
+		
 
 		trygetpixel(img, in_x, in_y)
 	end
@@ -146,9 +165,10 @@ end;
 # ╠═6b473b2d-4326-46b4-af38-07b61de287fc
 # ╟─2e8c4a48-d535-44ac-a1f1-4cb26c4aece6
 # ╟─c0c90fec-0e55-4be3-8ea2-88b8705ee258
-# ╟─60532aa0-740c-11eb-0402-af8ff117f042
-# ╟─8e0505be-359b-4459-9de3-f87ec7b60c23
-# ╟─35904b8e-7a28-4dbc-bbf9-b45da448452c
+# ╠═60532aa0-740c-11eb-0402-af8ff117f042
+# ╠═8e0505be-359b-4459-9de3-f87ec7b60c23
+# ╠═35904b8e-7a28-4dbc-bbf9-b45da448452c
+# ╠═e2c9600e-7613-11eb-0c5b-492127ac0817
 # ╟─f085296d-48b1-4db6-bb87-db863bb54049
 # ╟─d1757b2c-7400-11eb-1406-d937294d5388
 # ╟─c536dafb-4206-4689-ad6d-6935385d8fdf
