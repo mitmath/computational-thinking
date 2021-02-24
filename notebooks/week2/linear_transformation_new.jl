9717131bbd22cba75d09377669a0770e77ee3f77
Alan Edelman <edelman.alan@gmail.com>
2021-02-23 19:07:49 -0500

    Update linear_transformation_new.jl
    nonlinear corgis

--- notebooks/week2/linear_transformation_new.jl
+++ notebooks/week2/linear_transformation_new.jl
@@ -38,23 +38,52 @@ end
 # ╔═╡ 60532aa0-740c-11eb-0402-af8ff117f042
 md"Show grid lines $(@bind show_grid CheckBox(default=true))"
 
-# ╔═╡ 35904b8e-7a28-4dbc-bbf9-b45da448452c
+# ╔═╡ b0e5ebac-7631-11eb-0d7a-9b822cdb7011
+rθ(x::Vector) = ( norm(x), atan(x[2],x[1]))
+
+# ╔═╡ 766522d0-7632-11eb-17ce-4987172a198e
+xy((r,θ)) = ( r*cos(θ), r*sin(θ))
+
+# ╔═╡ 2efaa336-7630-11eb-0c17-a7d4a0141dac
+@bind z Slider(.1:.1:3, show_value=true)
+
+# ╔═╡ 488d732c-7631-11eb-38c3-e9a9165d5606
+	@bind f Slider(.1:1:3, show_value=true)
+
+
+# ╔═╡ 005ca75a-7622-11eb-2ba4-9f450e71df1f
 let
 
-	range = -2:.1:2
-	md"""
+range = -1.5:.1:1.5
+md"""
 	
-	a $(@bind a Slider(range; default=1.0, show_value=true))
-	
-	b $(@bind b Slider(range; default=0.0, show_value=true))
-	
-	c $(@bind c Slider(range; default=0.0, show_value=true))
-	
-	d $(@bind d Slider(range; default=1.0, show_value=true))
+a $(@bind a Scrubbable( range; default=1.0))
+b $(@bind b Scrubbable( range; default=0.0))
+
+c $(@bind c Scrubbable(range; default=0.0 ))
+d $(@bind d Scrubbable(range; default=1.0))
 	
 	**Re-run this cell to reset to identity transformation**
-	"""
+"""
 end
+
+# ╔═╡ 35904b8e-7a28-4dbc-bbf9-b45da448452c
+# let
+
+# 	range = -2:.1:2
+# 	md"""
+	
+# 	a $(@bind a Slider(range; default=1.0, show_value=true))
+	
+# 	b $(@bind b Slider(range; default=0.0, show_value=true))
+	
+# 	c $(@bind c Slider(range; default=0.0, show_value=true))
+	
+# 	d $(@bind d Slider(range; default=1.0, show_value=true))
+	
+# 	**Re-run this cell to reset to identity transformation**
+# 	"""
+# end
 
 # ╔═╡ f085296d-48b1-4db6-bb87-db863bb54049
 A = [
@@ -93,6 +122,12 @@ img_original = load(download(img_source));
 # ╔═╡ 52a8009e-761c-11eb-2dc9-dbccdc5e7886
 typeof(img_original)
 
+# ╔═╡ b754bae2-762f-11eb-1c6a-01251495a9bb
+begin
+	white(c::RGB) = RGB(1,1,1)
+	white(c::RGBA) = RGBA(1,1,1,0.75)
+end
+
 # ╔═╡ 7d0096ad-d89a-4ade-9679-6ee95f7d2044
 function trygetpixel(img::AbstractMatrix, x::Float64, y::Float64)
 	rows, cols = size(img)
@@ -100,14 +135,14 @@ function trygetpixel(img::AbstractMatrix, x::Float64, y::Float64)
 	"The linear map [-1,1] ↦ [0,1]"
 	f = t -> (t - -1.0)/(1.0 - -1.0)
 	
-	i = floor(Int, rows * f(-y))
-	j = floor(Int, cols * f(x * (rows / cols)))
+	i = floor(Int, rows *  f(-y) / z)
+	j = floor(Int, cols *  f(x * (rows / cols))  / z)
  
 	if 1 < i ≤ rows && 1 < j ≤ cols
 		img[i,j]
 	else
-		zero(eltype(img))
-		# RGBA(1, 1, 1, 0.75)
+		white(img[1,1])
+
 	end
 end
 
@@ -147,14 +182,19 @@ height, width = size(img)
 	if det_A == 0
 		RGB(1.0, 1.0, 1.0)
 	else
-		in_x, in_y = A \ [out_x, out_y]
-
+		
+		 # in_x, in_y = A \ [out_x, out_y]
+         in_x, in_y = xy( [out_x, out_y] )
+		
 		trygetpixel(img, in_x, in_y)
 	end
 	
-	for out_y in LinRange(2, -2, 300),
-		out_x in LinRange(-2, 2, 300)
+	for out_y in LinRange(f, -f, 500),
+		out_x in LinRange(-f, f, 500)
 ]
+
+# ╔═╡ 90b643fc-7633-11eb-3e6d-d911a2da4157
+img
 
 # ╔═╡ Cell order:
 # ╠═6b473b2d-4326-46b4-af38-07b61de287fc
@@ -162,15 +202,22 @@ height, width = size(img)
 # ╟─c0c90fec-0e55-4be3-8ea2-88b8705ee258
 # ╟─60532aa0-740c-11eb-0402-af8ff117f042
 # ╠═8d8ac24a-761d-11eb-1e93-9563dd88e74b
-# ╠═8e0505be-359b-4459-9de3-f87ec7b60c23
-# ╠═35904b8e-7a28-4dbc-bbf9-b45da448452c
+# ╟─8e0505be-359b-4459-9de3-f87ec7b60c23
+# ╠═90b643fc-7633-11eb-3e6d-d911a2da4157
+# ╠═b0e5ebac-7631-11eb-0d7a-9b822cdb7011
+# ╠═766522d0-7632-11eb-17ce-4987172a198e
+# ╠═2efaa336-7630-11eb-0c17-a7d4a0141dac
+# ╠═488d732c-7631-11eb-38c3-e9a9165d5606
+# ╟─005ca75a-7622-11eb-2ba4-9f450e71df1f
+# ╟─35904b8e-7a28-4dbc-bbf9-b45da448452c
 # ╟─f085296d-48b1-4db6-bb87-db863bb54049
 # ╟─d1757b2c-7400-11eb-1406-d937294d5388
 # ╟─c536dafb-4206-4689-ad6d-6935385d8fdf
 # ╟─fb509fb4-9608-421d-9c40-a4375f459b3f
-# ╠═40655bcc-6d1e-4d1e-9726-41eab98d8472
+# ╟─40655bcc-6d1e-4d1e-9726-41eab98d8472
 # ╠═4fcb4ac1-1ad1-406e-8776-4675c0fdbb43
 # ╠═52a8009e-761c-11eb-2dc9-dbccdc5e7886
 # ╠═55898e88-36a0-4f49-897f-e0850bd2b0df
 # ╠═7d0096ad-d89a-4ade-9679-6ee95f7d2044
+# ╠═b754bae2-762f-11eb-1c6a-01251495a9bb
 # ╠═83d45d42-7406-11eb-2a9c-e75efe62b12c
