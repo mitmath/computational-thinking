2b9c8f03c49083b194aa418091780462220faeb1
Alan Edelman <edelman.alan@gmail.com>
2021-03-01 05:34:55 -0500

    Update transformations2.jl
    added linear

--- notebooks/week3/transformations2.jl
+++ notebooks/week3/transformations2.jl
@@ -88,11 +88,6 @@ overflow-x: hidden;
 # ╔═╡ b7895bd2-7634-11eb-211e-ef876d23bd88
 PlutoUI.TableOfContents(aside=true)
 
-# ╔═╡ e0b657ce-7a03-11eb-1f9d-f32168cb5394
-md"""
-#  The fun stuff: playing with the transforms.
-"""
-
 # ╔═╡ ef3f9cb0-7a03-11eb-177f-65f281148496
 begin
 	
@@ -103,10 +98,42 @@ end
 
 # ╔═╡ 96766502-7a06-11eb-00cc-29849773dbcf
 
-img_original = load(download(corgis)); 
-#img_original = load(download(longcorgi));
+#img_original = load(download(corgis)); 
+img_original = load(download(longcorgi));
 #img_original = #load(download("https://news.mit.edu/sites/default/files/styles/news_article__image_gallery/public/images/202004/edelman%2520philip%2520sanders.png?itok=ZcYu9NFeg "));
 
+
+# ╔═╡ 26dd0e98-7a75-11eb-2196-5d7bda201b19
+md"""
+After you select your imaage, we suggest moving this line above just above the top of your browser.
+
+---------------
+"""
+
+# ╔═╡ e0b657ce-7a03-11eb-1f9d-f32168cb5394
+md"""
+#  The fun stuff: playing with the transforms.
+"""
+
+# ╔═╡ 005ca75a-7622-11eb-2ba4-9f450e71df1f
+let
+
+range = -1.5:.1:1.5
+md"""
+**A =**  
+	
+``(``	
+ $(@bind a Scrubbable( range; default=1.0))
+ $(@bind b Scrubbable( range; default=0.0))
+``)``
+
+``(``
+$(@bind c Scrubbable(range; default=0.0 ))
+$(@bind d Scrubbable(range; default=1.0)) 
+``)``  (This is a "scrubbable matrix" -- click on the number and drag to change.)	
+	
+"""
+end
 
 # ╔═╡ 45dccdec-7912-11eb-01b4-a97e30344f39
 md"Show grid lines $(@bind show_grid CheckBox(default=true))"
@@ -118,8 +145,16 @@ md"""
 
 # ╔═╡ 23ade8ee-7a09-11eb-0e40-296c6b831d74
 md"""
-Grab a linear or nonlinear transform (see TOC) or make up your own
+Grab a [linear](#a0afe3ae-76b9-11eb-2301-cde7260ddd7f) or [nonlinear](#a290d5e2-7a02-11eb-37db-41bf86b1f3b3) transform or make up your own
 """
+
+# ╔═╡ 55b5fc92-7a76-11eb-3fba-854c65eb87f9
+md"""
+Above: The original image is placed in a [-1,1] x [-1 1] box and transformed.
+"""
+
+# ╔═╡ 85686412-7a75-11eb-3d83-9f2f8a3c5509
+A = [a b ; c d];
 
 # ╔═╡ a7df7346-79f8-11eb-1de6-71f027c46643
 md"""
@@ -204,6 +239,16 @@ begin
 	 lin(A) = v-> A*[v...]  # abbreviation for the above		 	
 end
 
+# ╔═╡ 58a30e54-7a08-11eb-1c57-dfef0000255f
+#T = id
+T = rotate(α)
+# T = shear(α)
+#T = lin(A) # uses the scrubbable 
+## Checking some transformations and inverses
+#  T = shear(α) ∘ shear(-α)
+#  T = nonlin_shear(α)  ∘ nonlin_shear(-α)
+#  T  =  xy  ∘ rθ 
+
 # ╔═╡ a290d5e2-7a02-11eb-37db-41bf86b1f3b3
 md"""
 # Nonlinear Transformations (A collection)
@@ -220,16 +265,16 @@ begin
   # merc = ((x,y),) ->  [ log(x^2+y^2)/2 , atan(y,x) ] # (reim(log(complex(y,x)) ))
 end
 
-# ╔═╡ 58a30e54-7a08-11eb-1c57-dfef0000255f
- # T = shear(α) ∘ shear(-α)
-  T = nonlin_shear(α)  ∘ nonlin_shear(-α)
-# T = warp(α)
-#T  =  xy  ∘ rθ 
-
 # ╔═╡ 704a87ec-7a1e-11eb-3964-e102357a4d1f
 md"""
 # Composition
 """
+
+# ╔═╡ 4b0e8742-7a70-11eb-1e78-813f6ad005f4
+begin
+	x = rand()
+	( sin ∘ cos )(x) ≈ sin(cos(x))
+end
 
 # ╔═╡ 44792484-7a20-11eb-1c09-95b27b08bd34
 md"""
@@ -240,6 +285,21 @@ In math we *compose* two functions to create a new function, as
 in the function that takes $x$ to $\sin(\cos(x))$ is the composition
 of the sine function and the cosine function.  
 
+We humans tend to blur the distinction between the sine function
+and the value of $\sin(x)$ at some point $x$.  The sine function
+is a mathematical object by itself.  It's a thing that can be evaluated
+at as many $x$'s as you like.  
+
+If you look at the two sides of
+` (sin ∘ cos )(x) ≈ sin(cos(x)) ` and see that they are exactly the same, it's time to ask yourself what's a key difference? On the left a function is built
+` sin ∘ cos ` which is then evaluated at `x`.  On the right , that function
+is never built.
+"""
+
+
+
+# ╔═╡ f650b788-7a70-11eb-0b20-779d2f18f111
+md"""
 ## Composing Functions in computer science
 [wikipedia (cs)](https://en.wikipedia.org/wiki/Function_composition_(computer_science))
 
@@ -275,10 +335,41 @@ md"""
 md"""
 If $f$ is a function from 2-vectors to 2-vectors (say), we define the inverse function,
 $f^{-1}$ to have the property that $f(f^{-1}(v))=v$ and $f^{-1}(f(v))=v$.
+
+This equation might be true for all $v$ or for some $v$ in a region.   
 """
 
-# ╔═╡ 62c96656-7a1e-11eb-235f-a57e0d869c5c
+# ╔═╡ 7a4e785e-7a71-11eb-07fb-cfba453a117b
+md"""
+### Example: scaling up and down
+"""
 
+# ╔═╡ 9264508a-7a71-11eb-1b7c-bf6e62788115
+let
+	v = rand(2)
+	T = scale(2) ∘ scale(.5)
+	T(v) .≈ v 
+end
+
+# ╔═╡ e89339b2-7a71-11eb-0f97-971b2ed277d1
+let	
+	  T = scale(.5) ∘ scale(2)
+	
+	  v = rand(2)
+	  T(v) .≈ v 
+end
+
+# ╔═╡ 0957fd9a-7a72-11eb-0566-e93ef32fb626
+md"""
+We observe numerically that `scale(2)` and `scale(.5)` are inverse transformations.
+"""
+
+# ╔═╡ 014c14a6-7a72-11eb-119b-f5cfc82085ca
+md"""
+### Find your own examples.
+
+Take some of the Linear and Nonlinear Transformations (see TOC) and find some inverses by placing them in the `T=` section of "The fun stuff" at the top of this notebook.
+"""
 
 # ╔═╡ 89f0bc54-76bb-11eb-271b-3190b4d8cbc0
 md"""
@@ -316,6 +407,55 @@ warp(1)([5,6])
 # ╔═╡ 8e36f4a2-76bd-11eb-2fda-9d1424752812
 warp₂(1.0)([5.0,6.0])
 
+# ╔═╡ ad700740-7a74-11eb-3369-15e5fd89194d
+md"""
+# Linear Transformations: See a matrix, think beyond the number array.
+"""
+
+# ╔═╡ e051259a-7a74-11eb-12fc-99c5dc867fbd
+md"""
+Software writers and beginning linear algebra students see a matrix and see a lowly table of numbers.  We want you to see a *linear transformation*. That's what the professional mathematicians see.
+"""
+
+# ╔═╡ 1856ddae-7a78-11eb-3422-298e1103275b
+md"""
+What defines a linear transformation?  Well there are a few equivalent ways of giving a definition.
+"""
+
+# ╔═╡ 4b4fe818-7a78-11eb-2986-59e60063d346
+md"""
+**Linear Transformation Definitions:**
+"""
+
+# ╔═╡ 5d656494-7a78-11eb-12e8-d17856bd8c4d
+md"""
+	The intuitive definition:
+
+The rectangles (gridlines) in the transformed image [above](#e0b657ce-7a03-11eb-1f9d-f32168cb5394) always become a lattice of congruent parallelograms.
+
+	The easy operational (but devoid of intuition) definition: 
+
+
+A transformation is linear if it is defined by v->A*v (matrix times vector) for some fixed matrix A.
+
+	 The scaling and adding definition:
+
+1. If you scale and then transform or if you transform and then scale, the result is always the same:
+
+$T(cv)=cT(v)$ ( $v$ is any vector, and $c$ any number )
+
+2. If you add and then transform or vice versa the result is the same:
+
+$T(v_1+v_2) = Tv_1 + Tv_2.$ ($v_1,v_2$ are any vectors)
+
+	The mathematician's definition:
+
+(A consolidation of the above definition.) $T$ is linear if
+
+$T(c_1 v_1 + c_2 v_2) = c_1 Tv_1 + c_2 T v_2$ for all numbers $c_1,c_2$ and vectors $v_1,v_2$.  (this can be extended to beyond 2 terms.)
+
+"""
+
 # ╔═╡ ad728ee6-7639-11eb-0b23-c37f1366fb4e
 md"""
 ## 4.2 But what is a transformation, really? 
@@ -328,27 +468,6 @@ Congratulations, you now can do what computers excel at.
 md"""
 Matrices are often thought of as containers of numbers in a rectangular array, and hence one thinks of manipulating these tables like a spreadsheet, but actually the deeper meaning is that it is a transformation.
 """
-
-# ╔═╡ 005ca75a-7622-11eb-2ba4-9f450e71df1f
-let
-
-range = -1.5:.1:1.5
-md"""
-This is a "scrubbable matrix" -- click on the number and drag to change.	
-	
-``(``	
- $(@bind a Scrubbable( range; default=1.0))
- $(@bind b Scrubbable( range; default=0.0))
-``)``
-
-``(``
-$(@bind c Scrubbable(range; default=0.0 ))
-$(@bind d Scrubbable(range; default=1.0))
-``)``
-	
-	**Re-run this cell to reset to identity transformation**
-"""
-end
 
 # ╔═╡ 2efaa336-7630-11eb-0c17-a7d4a0141dac
 md"""
@@ -396,15 +515,6 @@ begin
 	inverse(f) = y -> inverse( (u, p) -> f(SVector(u[1],u[2])), y )
 end
 
-# ╔═╡ f085296d-48b1-4db6-bb87-db863bb54049
-A = [
-	a b
-	c d
-	]
-
-# ╔═╡ d1757b2c-7400-11eb-1406-d937294d5388
-md"**_Det(A)_ = $a * $d - $c * $b =  $(det(A))**"
-
 # ╔═╡ 5227afd0-7641-11eb-0065-918cb8538d55
 md"""
 We never seem to see this in linear algebra classes do we?
@@ -433,23 +543,6 @@ Solving 2 equations in 2 unknowns, and higher dimensional analogs.
 THe top 500 supercomputers, and how many equations in how many unknowns are being solved today.
 """
 
-# ╔═╡ a66eb6fe-76b3-11eb-1d50-659ec2bf7c44
-md"""
-### 4.4 Automatic Differentiation in 10 mins(ok 11)
-"""
-
-# ╔═╡ 62b28c02-763a-11eb-1418-c1e30555b1fa
-md"""
-What about more than 3 dimensions?
-
-Oh I've heard it all, 
-* there is no such thing as more than 3 dimensions. 
-* The fourth dimension is time, isn't it?  
-* I have trouble visualizing 3 dimensions, let alone contemplating 4.  
-
-... but mathematicians have no trouble with n spatial dimensions, and they do not (usually) worry about silly questions like whether these exist.
-"""
-
 # ╔═╡ c536dafb-4206-4689-ad6d-6935385d8fdf
 md"""
 ## Appendix
@@ -465,13 +558,6 @@ img_sources = [
 "https://previews.123rf.com/images/camptoloma/camptoloma2002/camptoloma200200020/140962183-pembroke-welsh-corgi-portrait-sitting-gray-background.jpg"=>"Portrait Corgi",
 	"https://www.eaieducation.com/images/products/506618_L.jpg"=>"Graph Paper"
 ]
-
-# ╔═╡ c0c90fec-0e55-4be3-8ea2-88b8705ee258
-md"""
-#### Choose an image:
-
-$(@bind img_source Select(img_sources))
-"""
 
 # ╔═╡ 4fcb4ac1-1ad1-406e-8776-4675c0fdbb43
 #img_original = load(download(img_source));
@@ -523,8 +609,8 @@ function with_gridlines(img::Array{<:Any,2}; n=20)
 	result[:, 1:sep_j:end] .= stroke
 
 	# a second time, to create a line 2 pixels wide
-	#result[2:sep_i:end, :] .= stroke
-	#result[:, 2:sep_j:end] .= stroke
+	result[2:sep_i:end, :] .= stroke
+	result[:, 2:sep_j:end] .= stroke
 	
 	 result[  sep_i * (n ÷2) .+ [1,2]    , :] .= RGBA(0,1,0,1)
 	result[ : ,  sep_j * (n ÷2) .+ [1,2]    ,] .= RGBA(1,0,0,1)
@@ -593,15 +679,19 @@ size(img)
 # ╟─972b2230-7634-11eb-028d-df7fc722ec70
 # ╟─6b473b2d-4326-46b4-af38-07b61de287fc
 # ╟─b7895bd2-7634-11eb-211e-ef876d23bd88
-# ╟─e0b657ce-7a03-11eb-1f9d-f32168cb5394
 # ╟─ef3f9cb0-7a03-11eb-177f-65f281148496
 # ╠═96766502-7a06-11eb-00cc-29849773dbcf
+# ╟─26dd0e98-7a75-11eb-2196-5d7bda201b19
+# ╟─e0b657ce-7a03-11eb-1f9d-f32168cb5394
+# ╟─005ca75a-7622-11eb-2ba4-9f450e71df1f
 # ╟─45dccdec-7912-11eb-01b4-a97e30344f39
 # ╟─ce55beee-7643-11eb-04bc-b517703facff
 # ╟─23ade8ee-7a09-11eb-0e40-296c6b831d74
 # ╠═58a30e54-7a08-11eb-1c57-dfef0000255f
 # ╟─f213ce72-7a06-11eb-0c81-f1cb6067fd30
+# ╠═55b5fc92-7a76-11eb-3fba-854c65eb87f9
 # ╠═7222a0f2-7a07-11eb-3560-3511fab319a2
+# ╟─85686412-7a75-11eb-3d83-9f2f8a3c5509
 # ╟─a7df7346-79f8-11eb-1de6-71f027c46643
 # ╟─044e6128-79fe-11eb-18c1-395ae857dc73
 # ╟─78d61e28-79f9-11eb-0605-e77d206cda84
@@ -614,10 +704,16 @@ size(img)
 # ╟─a290d5e2-7a02-11eb-37db-41bf86b1f3b3
 # ╠═b4cdd412-7a02-11eb-149a-df1888a0f465
 # ╟─704a87ec-7a1e-11eb-3964-e102357a4d1f
+# ╠═4b0e8742-7a70-11eb-1e78-813f6ad005f4
 # ╟─44792484-7a20-11eb-1c09-95b27b08bd34
+# ╟─f650b788-7a70-11eb-0b20-779d2f18f111
 # ╟─db4bc328-76bb-11eb-28dc-eb9df8892d01
 # ╟─0b8ed36c-7a1e-11eb-053c-63cf9ee0b16f
-# ╠═62c96656-7a1e-11eb-235f-a57e0d869c5c
+# ╟─7a4e785e-7a71-11eb-07fb-cfba453a117b
+# ╠═9264508a-7a71-11eb-1b7c-bf6e62788115
+# ╠═e89339b2-7a71-11eb-0f97-971b2ed277d1
+# ╟─0957fd9a-7a72-11eb-0566-e93ef32fb626
+# ╟─014c14a6-7a72-11eb-119b-f5cfc82085ca
 # ╠═89f0bc54-76bb-11eb-271b-3190b4d8cbc0
 # ╟─f70f7ea8-76b9-11eb-3bd7-87d40a2861b1
 # ╟─bf28c388-76bd-11eb-08a7-af2671218017
@@ -626,11 +722,14 @@ size(img)
 # ╠═70dc4346-7a03-11eb-055e-111d2519a44c
 # ╠═852592d6-76bd-11eb-1265-5f200e39113d
 # ╠═8e36f4a2-76bd-11eb-2fda-9d1424752812
-# ╟─ad728ee6-7639-11eb-0b23-c37f1366fb4e
+# ╟─ad700740-7a74-11eb-3369-15e5fd89194d
+# ╟─e051259a-7a74-11eb-12fc-99c5dc867fbd
+# ╠═1856ddae-7a78-11eb-3422-298e1103275b
+# ╟─4b4fe818-7a78-11eb-2986-59e60063d346
+# ╟─5d656494-7a78-11eb-12e8-d17856bd8c4d
+# ╠═ad728ee6-7639-11eb-0b23-c37f1366fb4e
 # ╟─4d4e6b32-763b-11eb-3021-8bc61ac07eea
 # ╟─2e8c4a48-d535-44ac-a1f1-4cb26c4aece6
-# ╠═c0c90fec-0e55-4be3-8ea2-88b8705ee258
-# ╟─005ca75a-7622-11eb-2ba4-9f450e71df1f
 # ╠═2efaa336-7630-11eb-0c17-a7d4a0141dac
 # ╠═7f28ac40-7914-11eb-1403-b7bec34aeb94
 # ╠═0897814e-793e-11eb-1483-354b81310eba
@@ -641,14 +740,10 @@ size(img)
 # ╠═67324636-7938-11eb-1afd-e7d5afa41954
 # ╠═62a9201c-7938-11eb-144c-15690c06be94
 # ╠═94520610-787f-11eb-278d-c1a092e5429e
-# ╟─f085296d-48b1-4db6-bb87-db863bb54049
-# ╟─d1757b2c-7400-11eb-1406-d937294d5388
 # ╟─5227afd0-7641-11eb-0065-918cb8538d55
 # ╟─2835e33a-7642-11eb-33fd-79fb8ad27fa7
 # ╟─4c93d784-763d-11eb-1f48-81d4d45d5ce0
 # ╟─559660cc-763d-11eb-1ed8-017b93ed4ecb
-# ╟─a66eb6fe-76b3-11eb-1d50-659ec2bf7c44
-# ╟─62b28c02-763a-11eb-1418-c1e30555b1fa
 # ╟─c536dafb-4206-4689-ad6d-6935385d8fdf
 # ╟─fb509fb4-9608-421d-9c40-a4375f459b3f
 # ╠═40655bcc-6d1e-4d1e-9726-41eab98d8472
