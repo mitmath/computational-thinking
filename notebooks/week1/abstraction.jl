9edafadf98548de80f0521b916641a6c865c0f99
Fons van der Plas <fonsvdplas@gmail.com>
2021-03-24 19:54:28 +0100

    Revert "Revert "Revert "Update abstraction.jl"""
    This reverts commit 87101bfc3671d237cd75548f4e00c20082ee0ad6.

--- notebooks/week1/abstraction.jl
+++ notebooks/week1/abstraction.jl
@@ -44,7 +44,7 @@ font-size: 1.5rem;
 opacity: .8;
 "><em>Section 1.2</em></p>
 <p style="text-align: center; font-size: 2rem;">
-<em> Intro to Abstractionzzzzzs </em>
+<em> Intro to Abstractions </em>
 </p>
 
 <p style="
