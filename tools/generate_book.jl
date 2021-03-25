f9eea547223f18fc4026fd6333b5e53f507bc43e
Fons van der Plas <fonsvdplas@gmail.com>
2021-03-25 13:46:58 +0100

    add julia version warning for generate_book.jl

--- tools/generate_book.jl
+++ tools/generate_book.jl
@@ -6,6 +6,10 @@ if !isdir("pluto-deployment-environment")
     """
 end
 
+if VERSION < v"1.6.0-aaa"
+   @error "Our website needs to be generated with Julia 1.6. Go to julialang.org/downloads to install it." 
+end
+
 import Pkg
 Pkg.activate("./pluto-deployment-environment")
 Pkg.instantiate()
