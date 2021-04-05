bd4cea5c45f57614badb7bdc72403b5f99e8db03
BolaSMalek <malek.bola@gmail.com>
2021-04-05 16:41:16 +0000

    Deploying to Spring21-franklin-output from  @ e8b6f21d1f229a2f1ca371794f1f230e7d7bf8f4 🚀

--- Spring21/notebooks/week8/random_walks_II.jl
+++ Spring21/notebooks/week8/random_walks_II.jl
@@ -13,6 +13,57 @@ macro bind(def, element)
     end
 end
 
+# ╔═╡ b58298b6-962c-11eb-0393-4bb4a4f02a79
+html"""
+<div style="
+position: absolute;
+width: calc(100% - 30px);
+border: 50vw solid #282936;
+border-top: 500px solid #282936;
+border-bottom: none;
+box-sizing: content-box;
+left: calc(-50vw + 15px);
+top: -500px;
+height: 500px;
+pointer-events: none;
+"></div>
+
+<div style="
+height: 500px;
+width: 100%;
+background: #282936;
+color: #fff;
+padding-top: 68px;
+">
+<span style="
+font-family: Vollkorn, serif;
+font-weight: 700;
+font-feature-settings: 'lnum', 'pnum';
+"> <p style="
+font-size: 1.5rem;
+opacity: .8;
+"><em>Section 2.6</em></p>
+<p style="text-align: center; font-size: 2rem;">
+<em> Random Walks II </em>
+</p>
+
+<p style="
+font-size: 1.5rem;
+text-align: center;
+opacity: .8;
+"><em>Lecture Video</em></p>
+<div style="display: flex; justify-content: center;">
+<div  notthestyle="position: relative; right: 0; top: 0; z-index: 300;">
+<iframe src="https://www.youtube.com/embed/" width=400 height=250  frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+</div>
+</div>
+
+<style>
+body {
+overflow-x: hidden;
+}
+</style>"""
+
 # ╔═╡ 85b45a43-d7bf-4597-a1a6-329b41dce20d
 begin
     import Pkg
@@ -458,6 +509,7 @@ xlabel!("t")
 end
 
 # ╔═╡ Cell order:
+# ╠═b58298b6-962c-11eb-0393-4bb4a4f02a79
 # ╠═85b45a43-d7bf-4597-a1a6-329b41dce20d
 # ╠═85c26eb4-c258-4a8b-9415-7b5f7ddff02a
 # ╟─2d48bfc6-9617-11eb-3a85-279bebd21332
@@ -484,7 +536,7 @@ end
 # ╠═999fb608-fb1a-46cb-82ca-f3f31fe617e1
 # ╟─6509e69a-6e50-4816-a98f-67ba437383fb
 # ╟─e58976a9-1784-441e-bb76-3011538b8ad0
-# ╠═1efc2b68-9313-424f-9850-eb4496cc8486
+# ╟─1efc2b68-9313-424f-9850-eb4496cc8486
 # ╟─6e93ffda-217b-4d46-86b5-534ddc1bae90
 # ╟─396d2490-3cb9-4f68-8fdf-9209d2010e02
 # ╟─dc1c22e8-1c7b-43b7-8421-c2ca708931a5
