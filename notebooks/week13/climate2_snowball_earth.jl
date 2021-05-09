88aa0148128c2071699361b5c4c3f1d2dab4088b
alanedelman <mit.edelman@gmail.com>
2021-05-09 23:51:28 +0000

    Deploying to Spring21-deploy from  @ 4a469c6c71590896605d9dd5a12f468db6a8c7ef 🚀

--- notebooks/week13/climate2_snowball_earth.jl
+++ notebooks/week13/climate2_snowball_earth.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.14.4
+# v0.14.5
 
 using Markdown
 using InteractiveUtils
@@ -22,31 +22,51 @@ TableOfContents(title="📚 Table of Contents", indent=true, depth=4, aside=true
 # ╔═╡ 61960e99-2932-4a8f-9e87-f64a7a043489
 md"""
 a = $(@bind a Slider(-5:.1:5, show_value=true, default=0) )
-b = $(@bind b Slider(-54:.01:10, show_value=true, default=0) )
 
 
-y₀ = $(@bind y₀ Slider(-1.5:.01:5, show_value=true, default=2.0) )
 """
 
 # ╔═╡ 6139554e-c6c9-4252-9d64-042074f68391
-#f(y,(a,b),t) =  c*((y<7.55) ? b : 0 ) + (14-y)
-
-f(y,(a,b),t) =  ((y<0) ? -1 : 1 )  - y + a
-
-# ╔═╡ a09a0064-c6ab-4912-bc9f-96ab72b8bbca
-sol = solve(  ODEProblem( f, y₀, (0,10.0), (a,b) ) );
+f(y,a,t) =   sign(y)  + a -  y  # zeros at ±1 + a
 
 # ╔═╡ bd65e7f9-ecf2-43ac-b5a8-99b03866a5c8
 begin
-	plot(y->f(y,(a,b),0), -2,-.01, xlabel="Temperature" , ylabel="dT/dt")
-	plot!(y->f(y,(a,b),0), 0,2, xlabel="Temperature" , ylabel="dT/dt")
+	plot( y->f(y,a, 0), -9, -eps(), xlabel="y" , ylabel="sign(y)-y+a", lw=3,color=:blue)
+	plot!(y->f(y,a, 0), eps() ,9, lw=3, color=:blue)
+	ylims!(-5,10)
+	xlims!(-9,9)
+	hline!([0], ls=:dash, color=:pink, legend=false)
+    
 	
-	hline!([0])
+	if a< 1
+	 
+	  annotate!( -1+a, 1, text(round(-1+a,digits=1), position=:center))
+	  scatter!([-1+a],[0],m=:circle,ms=10,color=:blue)
+	end
+	if a>-1
+	  
+	  annotate!( 1+a, 1, text(round(1+a,digits=1), position=:center))
+		 scatter!([1+a],[0],m=:circle,ms=10,color=:blue)
+	end
+	vline!([ 1+a], ls=:dash, color=:gray)
+	 vline!([-1+a], ls=:dash, color=:gray)
+	vline!([0],ls=:dash, color=:pink)
 end
 
+# ╔═╡ 465f637c-0555-498b-a881-a2f6e5714cbb
+md"""
+y₀ = $(@bind y₀ Slider(-1.5:.01:5, show_value=true, default=2.0) )
+"""
+
+# ╔═╡ a09a0064-c6ab-4912-bc9f-96ab72b8bbca
+sol = solve(  ODEProblem( f, y₀, (0,10.0), a ));
+
 # ╔═╡ f8ee2373-6af0-4d81-98fb-23bde10198ef
-begin
-	p = plot( sol , legend=false, background_color_inside=:black , ylims=(-1.5+a,1.5+a), lw=3, c=:red)
+function plotit(y₀, a)
+	
+	sol = solve(  ODEProblem( f, y₀, (0,10.0), a ));
+	p = plot( sol , legend=false, background_color_inside=:black , ylims=(-7,7), lw=3, c=:red)
+	
 	# plot direction field:
 		xs = Float64[]
 		ys = Float64[]
@@ -54,7 +74,7 @@ begin
 	    lrx = LinRange( xlims(p)..., 30)
 		for x in  lrx
 			for y in LinRange( ylims(p)..., 30)
-				v = [1, f(y, (a,b) ,x) ]
+				v = [1, f(y, a ,x) ]
 				v ./=  (100* (lrx[2]-lrx[1]))
 	
 				
@@ -63,20 +83,85 @@ begin
 			
 			end
 		end
-	if a<40 hline!( [-1+a],c=:white,ls=:dash); end
-	    hline!( [1+a],c=:white,ls=:dash)
+	
+	if a<1 hline!( [-1+a],c=:white,ls=:dash); end
+	if a>-1  hline!( [1+a],c=:white,ls=:dash);end
 	    hline!( [0 ],c=:pink,ls=:dash)
 		plot!(xs, ys, alpha=0.7, c=:yellow)
 	    ylabel!("y")
 	    annotate!(-.5,y₀,text("y₀",color=:red))
-	    title!("Solution to y'(t) = a - by")
+	if a>-1
+	   annotate!(5,1+a,text(round(1+a,digits=3),color=:white))
+	end
+	if a<1
+	   annotate!(5,-1+a,text(round(-1+a,digits=3),color=:white))
+	end
+
+return(p)
+end
+
+# ╔═╡ a4686bca-90d6-4e02-961c-59f08fc37553
+plotit(y₀, a)
+
+# ╔═╡ fd882095-6cc4-4927-967c-6b02d5b1ad95
+let
+    Δt = 10
+    t = 0.0
+	y₀ =-5
+	a = -4
+	sol = solve(  ODEProblem( f, y₀, (t,t+Δt), a ))
+	p=plot(sol)
+	annotate!(10,-5,text("a=",7))
+	
+	
+	
+	for i= 1:32
+		a += .25
+		t += Δt
+		y₀ = sol(t)
+		sol = solve(  ODEProblem( f, y₀, (t,t+Δt), a ))
+		
+		if -1≤a ≤1
+		  p=plot!(sol,xlims=(0,t), legend=false, fillrange=-5,fillcolor=:gray,alpha=.5)
+		else
+		  p=plot!(sol,xlims=(0,t), legend=false)
+		end
+		
+		if i%4==0
+			annotate!(t,-5,text( round(a,digits=1), 7, :blue))
+		end
+		
+	
+	end
+	for i= 1:32
+		a -= .25
+		t += Δt
+		y₀ = sol(t)
+		sol = solve(  ODEProblem( f, y₀, (t,t+Δt), a ))
+		
+		if -1≤a≤1
+		  p=plot!(sol,xlims=(0,t), legend=false, fillrange=-5,fillcolor=:gray,alpha=.5)
+		else
+		  p=plot!(sol,xlims=(0,t), legend=false)
+		end
+		
+		if i%4==0
+			annotate!(t,-5,text( round(a,digits=1), 7, :red))
+		end
+	end
+	ylabel!("y")
+	
+	as_svg(plot!())
 end
 
 # ╔═╡ Cell order:
 # ╠═a0b3813e-adab-11eb-2983-616cf2bb6f5e
 # ╠═ef8d6690-720d-4772-a41f-b260d306b5b2
-# ╠═61960e99-2932-4a8f-9e87-f64a7a043489
-# ╟─6139554e-c6c9-4252-9d64-042074f68391
+# ╟─61960e99-2932-4a8f-9e87-f64a7a043489
+# ╠═6139554e-c6c9-4252-9d64-042074f68391
 # ╠═a09a0064-c6ab-4912-bc9f-96ab72b8bbca
-# ╠═bd65e7f9-ecf2-43ac-b5a8-99b03866a5c8
+# ╟─bd65e7f9-ecf2-43ac-b5a8-99b03866a5c8
+# ╟─465f637c-0555-498b-a881-a2f6e5714cbb
+# ╠═a4686bca-90d6-4e02-961c-59f08fc37553
 # ╠═f8ee2373-6af0-4d81-98fb-23bde10198ef
+# ╠═fd882095-6cc4-4927-967c-6b02d5b1ad95
