8100d7655279ef6fb6ae79408d357eb7b2bc3bd0
fonsp <fonsvdplas@gmail.com>
2022-12-12 16:20:36 +0000

    Deploying to Fall22-output from  @ 631a45a1e9f116de89ddc5debc698cf35f4e6745 🚀

--- Fall22/assets/scripts/sidebar.js
+++ Fall22/assets/scripts/sidebar.js
@@ -2,18 +2,15 @@ const sidebar = document.querySelector("#pages-sidebar")
 const layout = document.querySelector("#pages-layout")
 const navtoggle = document.querySelector("#toggle-nav")
 
-document.querySelector("#toggle-nav").addEventListener("click", function(e) {
+document.querySelector("#toggle-nav").addEventListener("click", function (e) {
     console.log(e)
-    layout.classList.toggle("pages_show_sidebar");
+    layout.classList.toggle("pages_show_sidebar")
     e.stopPropagation()
 })
 
-window.addEventListener("click", function(e) {
-    if(
-        !sidebar.contains(e.target) &&
-        !navtoggle.contains(e.target)
-        ) {
-        layout.classList.remove("pages_show_sidebar");
+window.addEventListener("click", function (e) {
+    if (!sidebar.contains(e.target) && !navtoggle.contains(e.target)) {
+        layout.classList.remove("pages_show_sidebar")
     }
 })
 
